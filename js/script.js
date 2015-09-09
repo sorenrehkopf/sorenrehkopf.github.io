14155e21c8b230707521c40a157e005b1f2c3602
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-09-08 19:57:13 -0700

    background styling updated

--- js/script.js
+++ js/script.js
@@ -6,63 +6,63 @@ $(function(){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()-20);
-		$('.about').css('text-shadow','7px 0px 5px');
-		$('.projects').css('text-shadow','0px 0px 0px');
-		$('.contact').css('text-shadow','0px 0px 0px');
-		$('.top').css('text-shadow','0px 0px 0px');
+		$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 	});
 
 	$('.projects').on('click',function(e){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()+$('#about').height()-20);
-		$('.about').css('text-shadow','0px 0px 0px');
-		$('.projects').css('text-shadow','7px 0px 5px');
-		$('.contact').css('text-shadow','0px 0px 0px');
-		$('.top').css('text-shadow','0px 0px 0px');
+		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.projects').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 	});
 
 	$('.contact').on('click',function(e){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()+$('#about').height()+$('#contact').height()-20);
-		$('.about').css('text-shadow','0px 0px 0px');
-		$('.projects').css('text-shadow','0px 0px 0px');
-		$('.contact').css('text-shadow','7px 0px 5px');
-		$('.top').css('text-shadow','0px 0px 0px');
+		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.contact').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 	});
 
 	$('.top').on('click',function(e){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop(0);
-		$('.about').css('text-shadow','0px 0px 0px');
-		$('.projects').css('text-shadow','0px 0px 0px');
-		$('.contact').css('text-shadow','0px 0px 0px');
-		$('.top').css('text-shadow','7px 0px 5px');
+		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('.top').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 	});
 
 	$(window).on('scroll',function(){
 		if($('body').scrollTop()>($('.header').height()+$('#about').height()+$('#projects').height()-200)){
-			$('.about').css('text-shadow','0px 0px 0px');
-			$('.projects').css('text-shadow','0px 0px 0px');
-			$('.contact').css('text-shadow','7px 0px 5px');
-			$('.top').css('text-shadow','0px 0px 0px');
+			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.contact').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		}else if($('body').scrollTop()>($('.header').height()+$('#about').height()-200)){
-			$('.about').css('text-shadow','0px 0px 0px');
-			$('.projects').css('text-shadow','7px 0px 5px');
-			$('.contact').css('text-shadow','0px 0px 0px');
-			$('.top').css('text-shadow','0px 0px 0px');
+			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.projects').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		}else if($('body').scrollTop()>($('.header').height()-200)){
-			$('.about').css('text-shadow','7px 0px 5px');
-			$('.projects').css('text-shadow','0px 0px 0px');
-			$('.contact').css('text-shadow','0px 0px 0px');
-			$('.top').css('text-shadow','0px 0px 0px');
+			$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		}else{
-			$('.about').css('text-shadow','0px 0px 0px');
-			$('.projects').css('text-shadow','0px 0px 0px');
-			$('.contact').css('text-shadow','0px 0px 0px');
-			$('.top').css('text-shadow','7px 0px 5px');
+			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+			$('.top').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 		}
 	});
 
