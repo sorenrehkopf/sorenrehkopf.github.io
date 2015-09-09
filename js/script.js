a0ddb43b5f0a3b61d50bf15dd9a9b1b228daedb8
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-09-08 23:59:23 -0700

    style changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(function(){
 
+	$('#nav').fadeOut(0);
 	
 	$('.about').on('click',function(e){
 
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()-20);
+		$('#nav').fadeIn();
 		$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
@@ -16,6 +18,7 @@ $(function(){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()+$('#about').height()-20);
+		$('#nav').fadeIn();
 		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.projects').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
@@ -26,6 +29,7 @@ $(function(){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop($('.header').height()+$('#about').height()+$('#contact').height()-20);
+		$('#nav').fadeIn();
 		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.contact').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
@@ -36,6 +40,7 @@ $(function(){
 		e.preventDefault();
 		// console.log('wow!!')
 		$('body').scrollTop(0);
+		$('#nav').fadeOut();
 		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
@@ -54,16 +59,35 @@ $(function(){
 			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		}else if($('body').scrollTop()>($('.header').height()-200)){
+			$('#nav').fadeIn(600);
 			$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 		}else{
+			$('#nav').fadeOut();
 			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.top').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 		}
 	});
+
+	$('.resume').on('click','a',function(e){
+		e.preventDefault();
+		console.log('click')
+		$('body').css('overflow','hidden');	
+		$('#resClose').css('display','block');
+		$('#modalBlock').css('display','block');
+		$('#resShow').css('display','block');	
+	});	
+	$('#resClose').on('click',function(e){
+		e.preventDefault();
+		console.log('click')
+		$('body').css('overflow','auto');
+		$('#resClose').css('display','none');	
+		$('#modalBlock').css('display','none');
+		$('#resShow').css('display','none');	
+	});	
 
 });
