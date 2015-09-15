b24ec8b8860e98233e25a8972ac9947808048e3d
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-09-15 14:14:04 -0700

    jQuery compatibility for ie and firefox added

--- js/script.js
+++ js/script.js
@@ -3,62 +3,39 @@ $(function(){
 	$('#nav').fadeOut(0);
 	
 	$('.about').on('click',function(e){
-
 		e.preventDefault();
-		// console.log('wow!!')
-		$('body').scrollTop($('.header').height()-20);
-		$('#nav').fadeIn();
-		$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
-		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('body,html').animate({scrollTop:$('.header').height()-20},300);
 	});
 
 	$('.projects').on('click',function(e){
 		e.preventDefault();
-		// console.log('wow!!')
-		$('body').scrollTop($('.header').height()+$('#about').height()-20);
-		$('#nav').fadeIn();
-		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.projects').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
-		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('body,html').animate({scrollTop:$('.header').height()+$('#about').height()-20},300);
 	});
 
 	$('.contact').on('click',function(e){
 		e.preventDefault();
-		// console.log('wow!!')
-		$('body').scrollTop($('.header').height()+$('#about').height()+$('#contact').height()-20);
-		$('#nav').fadeIn();
-		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.contact').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
-		$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
+		$('body,html').animate({scrollTop:$('.header').height()+$('#about').height()+$('#contact').height()-20},300);
 	});
 
 	$('.top').on('click',function(e){
 		e.preventDefault();
-		// console.log('wow!!')
-		$('body').scrollTop(0);
-		$('#nav').fadeOut();
-		$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		$('.top').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
+		$('body,html').animate({scrollTop:0},400);
 	});
 
 	$(window).on('scroll',function(){
-		if($('body').scrollTop()>($('.header').height()+$('#about').height()+$('#projects').height()-200)){
+		if($('body').scrollTop()>($('.header').height()+$('#about').height()+$('#projects').height()-200)||$('html').scrollTop()>($('.header').height()+$('#about').height()+$('#projects').height()-200)){
+			$('#nav').fadeIn(600);
 			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.contact').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		}else if($('body').scrollTop()>($('.header').height()+$('#about').height()-200)){
+		}else if($('body').scrollTop()>($('.header').height()+$('#about').height()-200)||$('html').scrollTop()>($('.header').height()+$('#about').height()-200)){
+			$('#nav').fadeIn(600);
 			$('.about').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.projects').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 			$('.contact').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
 			$('.top').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
-		}else if($('body').scrollTop()>($('.header').height()-200)){
+		}else if($('body').scrollTop()>($('.header').height()-200)||$('html').scrollTop()>($('.header').height()-200)){
 			$('#nav').fadeIn(600);
 			$('.about').css({'text-shadow':'7px 0px 5px','font-size':'19px'});
 			$('.projects').css({'text-shadow':'0px 0px 0px','font-size':'17px'});
@@ -76,7 +53,7 @@ $(function(){
 	$('.resume').on('click','a',function(e){
 		e.preventDefault();
 		console.log('click')
-		$('body').css('overflow','hidden');	
+		$('body,html').css('overflow','hidden');	
 		$('#resClose').css('display','block');
 		$('#modalBlock').css('display','block');
 		$('#resShow').css('display','block');	
@@ -84,7 +61,7 @@ $(function(){
 	$('#resClose').on('click',function(e){
 		e.preventDefault();
 		console.log('click')
-		$('body').css('overflow','auto');
+		$('body,html').css('overflow','auto');
 		$('#resClose').css('display','none');	
 		$('#modalBlock').css('display','none');
 		$('#resShow').css('display','none');	
